9e7a78a68a26dda54ee18027f0a67852fcc39f7b
Sarvottamananda (smoke) <sarvottamananda@gmail.com>
2020-09-19 00:08:26 +0530

    Deleted generated .deps

--- minimal_flat/clean-all.sh
+++ minimal_flat/clean-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm -rf autom4te.cache
+rm -rf autom4te.cache .deps
 rm aclocal.m4 compile config.log config.status configure\
     depcomp install-sh Makefile Makefile.in minimal_flat minimal_flat.o missing 
